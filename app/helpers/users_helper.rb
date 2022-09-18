0f328bd26e48151861f233b2be11cdffc69a70c2
Jim Jones <jim.jones1@gmail.com>
2022-09-17 20:34:58 -0500

    UI changes.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def debug_view
+    link_to("Checkpoint Debugger View", "/checkpoint",
+            class: 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded',
+            target: '_blank')
+  end
 end
